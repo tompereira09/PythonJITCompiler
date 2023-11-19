85b37716fb569ff5d739d729cadf30bd6652af78
3faze <93428357+3faze@users.noreply.github.com>
2023-11-19 13:38:47 +0000

    Add files via upload

--- lexer.py
+++ lexer.py
@@ -1,8 +1,7 @@
 class Token:
-    def __init__(self):
-        self.ty = None
-        self.value = None
-        self.start_pos = 0
+    def __init__(self, ty, value):
+        self.ty = ty
+        self.value = value
         self.end_pos = 0
 
 
@@ -38,138 +37,33 @@ def clearToken(token: Token):
 
 
 class Tokenizer:
-    def __init__(self):
-        self.token_obj = Token()
-
-    def tokenize(self, src: str):
-        clearToken(self.token_obj)
-        pos = 0
+    def tokenize(self, src: str, pos : int):
         c = nth(pos, src)
-        if is_ident_char(c, True):
-            start = pos
-            while is_ident_char(get_next(pos, src), False):
-                pos += 1
-            end = pos + 1
-            self.token_obj.ty = "IDENT"
-            self.token_obj.value = src[start:end]
-            self.token_obj.end_pos = end
-            return self.token_obj
+        if c == "+":
+            #print("+")
+            obj = Token("PLUS", None)
+            obj.end_pos = pos + 1
+            return obj
 
         if c.isnumeric():
-            start = pos
-            while get_next(pos, src).isnumeric():
-                pos += 1
-            end = pos + 1
-            self.token_obj.ty = "NUMBER"
-            self.token_obj.value = src[start:end]
-            self.token_obj.end_pos = end
-            return self.token_obj
-
-        if c == "'" or c == '"':
-            start = pos + 1
-            while get_next(pos, src) != "'" and get_next(pos, src) != '"':
-                if pos >= len(src):
-                    print("Unfinished String"),
-                    quit()
-                pos += 1
-            end = pos + 1
-
-            self.token_obj.ty = "STR"
-            self.token_obj.value = src[start:end]
-            self.token_obj.end_pos = end + 1
-            return self.token_obj
+            #print(c)
+            end = pos
+            while c.isnumeric():
+                end += 1
+                c = nth(end, src)
+            obj = Token("INT", src[pos:end])
+            obj.end_pos = end
+            #print(obj.end_pos)
+            return obj
 
         if c == " ":
-            self.token_obj.ty = "BLANK"
-            self.token_obj.end_pos = pos + 1
-            return self.token_obj
+            obj = Token("BLANK", None)
+            obj.end_pos = pos + 1
+            return obj
 
-        if c == "(":
-            self.token_obj.ty = "LPAREN"
-            self.token_obj.end_pos = pos + 1
-            return self.token_obj
+        if pos <= len(src):
+            obj = Token("EOF", None)
+            obj.end_pos = pos + 1
+            return obj
 
-        if c == ")":
-            self.token_obj.ty = "RPAREN"
-            self.token_obj.end_pos = pos + 1
-            return self.token_obj
 
-        if c == "\0":
-            self.token_obj.ty = "EOF"
-            self.token_obj.end_pos = pos + 1
-            return self.token_obj
-
-        if c == "+":
-            if get_next(pos, src) == "=":
-                self.token_obj.ty = "PLUS_EQ"
-                self.token_obj.end_pos = pos + 2
-                return self.token_obj
-            self.token_obj.ty = "PLUS"
-            self.token_obj.end_pos = pos + 1
-            return self.token_obj
-
-        if c == "-":
-            if get_next(pos, src) == "=":
-                self.token_obj.ty = "MINUS_EQ"
-                self.token_obj.end_pos = pos + 2
-                return self.token_obj
-            self.token_obj.ty = "MINUS"
-            self.token_obj.end_pos = pos + 1
-            return self.token_obj
-
-        if c == "/":
-            if get_next(pos, src) == "=":
-                self.token_obj.ty = "DIV_EQ"
-                self.token_obj.end_pos = pos + 2
-                return self.token_obj
-            self.token_obj.ty = "DIV"
-            self.token_obj.end_pos = pos + 1
-            return self.token_obj
-
-        if c == "*":
-            if get_next(pos, src) == "=":
-                self.token_obj.ty = "MUL_EQ"
-                self.token_obj.end_pos = pos + 2
-                return self.token_obj
-            self.token_obj.ty = "MUL"
-            self.token_obj.end_pos = pos + 1
-            return self.token_obj
-
-        if c == "=":
-            if get_next(pos, src) == "=":
-                self.token_obj.ty = "DOUBLE_EQ"
-                self.token_obj.end_pos = pos + 2
-                return self.token_obj
-            self.token_obj.ty = "EQ"
-            self.token_obj.end_pos = pos + 1
-            return self.token_obj
-
-        if c == ">":
-            if get_next(pos, src) == "=":
-                self.token_obj.ty = "GREATER_EQ"
-                self.token_obj.end_pos = pos + 2
-                return self.token_obj
-
-            if get_next(pos, src) == ">":
-                self.token_obj.ty = "DOUBLE_GREATER"
-                self.token_obj.end_pos = pos + 2
-                return self.token_obj
-
-            self.token_obj.ty = "GREATER"
-            self.token_obj.end_pos = pos + 1
-            return self.token_obj
-
-        if c == "<":
-            if get_next(pos, src) == "=":
-                self.token_obj.ty = "SMALLER_EQ"
-                self.token_obj.end_pos = pos + 2
-                return self.token_obj
-
-            if get_next(pos, src) == "<":
-                self.token_obj.ty = "DOUBLE_SMALLER"
-                self.token_obj.end_pos = pos + 2
-                return self.token_obj
-
-            self.token_obj.ty = "SMALLER"
-            self.token_obj.end_pos = pos + 1
-            return self.token_obj
--- parser.py
+++ parser.py
@@ -1,30 +1,22 @@
-# IMPORTS
-import lexer
+####### IMPORTS
+from lexer import *
+###############
+
 
 tokens = []
-
-tokenizer = lexer.Tokenizer()
-input_string = "1 + 2"
-obj = tokenizer.tokenize(input_string)
+tokenizer = Tokenizer()
+inpstr = "123 + 23 + 1"
+inpoint = 0
+obj = tokenizer.tokenize(inpstr, inpoint)
+#print(obj.ty, obj.value)
 tokens.append(obj)
+inpoint = obj.end_pos
 while obj.ty != "EOF":
-    input_string = input_string[obj.end_pos:]
-#    print(obj.ty, obj.value)
+    obj = tokenizer.tokenize(inpstr, inpoint)
+    #print(obj.ty, obj.value)
     tokens.append(obj)
-    obj = tokenizer.tokenize(input_string)
+    inpoint = obj.end_pos
 
-class NumberToken:
-    def __init__(self, tok):
-        self.tok = tok
+for i in tokens:
+    print(i.ty, i.value)
 
-    def __repr__(self):
-        return f'{self.tok.value}'
-
-class OpToken:
-    def __init__(self):
-        self.op = ""
-        self.left = None
-        self.right = None
-
-    def __repr__(self):
-        return f'({self.left} {self.op} {self.right})'
